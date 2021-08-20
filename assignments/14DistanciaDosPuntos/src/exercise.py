fc9bf9ced6c1b13a7af229b650d9496b8c5365ab
SofiaMejia <A01252540@itesm.mx>
2021-08-20 16:52:40 +0000

    14 distancia dos puntos

--- assignments/14DistanciaDosPuntos/src/exercise.py
+++ assignments/14DistanciaDosPuntos/src/exercise.py
@@ -1,6 +1,11 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
-
+    x1 = int(input('x1= '))
+    y1 = int(input('y1= '))
+    x2 = int(input('x2= '))
+    y2 = int(input('y2= '))
+    distancia = float(math.sqrt(((x2-x1)**2) + ((y2-y1)**2)))
+    print (f"distancia= {distancia:.2f}")
 if __name__ == '__main__':
     main()
